d8af29db5c65464dddbe4f72ce12e34352d047a8
Andrey-Grebnev <a.grebnev@gwd.ru>
2025-10-31 10:57:11 +0300

    57

--- src/components/client/SerchSort.jsx
+++ src/components/client/SerchSort.jsx
@@ -218,7 +218,7 @@ const SerchSort = () => {
                         </div>
                         <input
                             className="text-black outline-none flex-1 text-xs md:text-lg"
-                            placeholder="Поиск по названию...++10+32"
+                            placeholder="Поиск по названию...++10+32 57"
                             value={serchData.text}
                             onChange={(e) => {
                                 toggleText(e.target.value);
