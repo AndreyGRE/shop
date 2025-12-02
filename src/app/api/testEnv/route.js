7b13b6538aa50e18ac72cbe26638aea5d1c1013f
Andrey-Grebnev <a.grebnev@gwd.ru>
2025-12-02 16:24:56 +0300

    c

--- src/app/api/testEnv/route.js
+++ src/app/api/testEnv/route.js
@@ -4,5 +4,6 @@ export async function GET() {
     return NextResponse.json({
         shopId: process.env.YOOKASSA_SHOP_ID || "undefined",
         secretKey: process.env.YOOKASSA_SECRET_KEY_TEST || "undefined",
+        secretKe: process.env.YOOKASSA_SECRET_KEY || "undefined",
     });
 }
